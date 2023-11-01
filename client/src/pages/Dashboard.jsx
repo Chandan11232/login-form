d62b96faba510c0cb278e824813c24d3deb0cd53
Chandan11232 <cgarg2461@gmail.com>
2023-11-01 08:50:39 +0530

    updated dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,15 +1,22 @@
 import { useContext } from "react";
 import { UserContext } from "../../context/userContext";
+import { useNavigate } from "react-router-dom";
 
 const Dashboard = () => {
+  const navigate = useNavigate();
   const { user } = useContext(UserContext);
-  return (
-    <div>
-      <h1>Dashboard</h1>
-      {!!user && <h2>Hi {user.name}!</h2>}
-      {console.log(user.name)}
-    </div>
-  );
+  if (!user) {
+    navigate("/login");
+    return null;
+  }
+  if (user) {
+    return (
+      <div>
+        <h1>Dashboard</h1>
+        {!!user && <h2>Hi {user.name}!</h2>}
+      </div>
+    );
+  }
 };
 
 export default Dashboard;
